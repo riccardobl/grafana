f8cb54162d22c8e8473d0fa2a2c98265426c2cc6
gamab <gabi.mabs@gmail.com>
2021-08-19 16:39:06 +0200

    Adding accessControl to the ButtonRow component

--- public/app/features/datasources/settings/ButtonRow.tsx
+++ public/app/features/datasources/settings/ButtonRow.tsx
@@ -4,6 +4,9 @@ import { selectors } from '@grafana/e2e-selectors';
 import config from 'app/core/config';
 import { Button, LinkButton } from '@grafana/ui';
 
+import { AccessControlAction } from 'app/types/';
+import { contextSrv } from 'app/core/core';
+
 export interface Props {
   isReadOnly: boolean;
   onDelete: () => void;
@@ -12,21 +15,26 @@ export interface Props {
 }
 
 const ButtonRow: FC<Props> = ({ isReadOnly, onDelete, onSubmit, onTest }) => {
+  const canEditDataSources = !isReadOnly && contextSrv.hasPermission(AccessControlAction.DataSourcesWrite);
+  const canDeleteDataSources = contextSrv.hasPermission(AccessControlAction.DataSourcesDelete);
+
   return (
     <div className="gf-form-button-row">
       <LinkButton variant="secondary" fill="outline" href={`${config.appSubUrl}/datasources`}>
         Back
       </LinkButton>
-      <Button
-        type="button"
-        variant="destructive"
-        disabled={isReadOnly}
-        onClick={onDelete}
-        aria-label={selectors.pages.DataSource.delete}
-      >
-        Delete
-      </Button>
-      {!isReadOnly && (
+      {canDeleteDataSources && (
+        <Button
+          type="button"
+          variant="destructive"
+          disabled={isReadOnly}
+          onClick={onDelete}
+          aria-label={selectors.pages.DataSource.delete}
+        >
+          Delete
+        </Button>
+      )}
+      {canEditDataSources && (
         <Button
           type="submit"
           variant="primary"
@@ -37,7 +45,7 @@ const ButtonRow: FC<Props> = ({ isReadOnly, onDelete, onSubmit, onTest }) => {
           Save &amp; test
         </Button>
       )}
-      {isReadOnly && (
+      {!canEditDataSources && (
         <Button type="submit" variant="primary" onClick={onTest}>
           Test
         </Button>
